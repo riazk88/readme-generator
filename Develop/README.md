d4ed62d7fb4fbee4c63cd917343550f89a882705
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-05-17 09:41:24 -0600

    hashtags in readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,34 @@
-undefined
+
+# Title
+readme-generator
+    
+## Description
+gen a readme
+
+## Table of Contents
+* [Installation] (#installation)
+* [Usage] (#usage)
+* [Contribution] (#contribution)
+* [Tests] (#tests)
+* [License] (#license)
+
+## Installation
+node
+
+## Usage
+none
+
+## Contribution
+Kiran
+
+## Tests
+none
+
+## License
+This application is covered under the MIT license.
+
+## Questions
+Please reach out for additional questions!
+GitHub Username: riazk88
+GitHub Page: https://github.com/riazk88
+Email: riazk88@yahoo.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,40 +1,39 @@
 function generateReadme (data) {
-    return 
-
-    `# Title
-    ${data.title}
+return `
+# Title
+${data.title}
     
-    ## Description
-    ${data.description}
+## Description
+${data.description}
 
-    ## Table of Contents
-    * [Installation] (#installation)
-    * [Usage] (#usage)
-    * [Contribution] (#contribution)
-    * [Tests] (#tests)
-    * [License] (#license)
+## Table of Contents
+* [Installation] (#installation)
+* [Usage] (#usage)
+* [Contribution] (#contribution)
+* [Tests] (#tests)
+* [License] (#license)
 
-    ## Installation
-    ${data.installation}
+## Installation
+${data.installation}
 
-    ## Usage
-    ${data.usage}
+## Usage
+${data.usage}
 
-    ## Contribution
-    ${data.contribution}
+## Contribution
+${data.contribution}
 
-    ## Tests
-    ${data.tests}
+## Tests
+${data.tests}
 
-    ## License
-    This application is covered under the ${data.license} license.
+## License
+This application is covered under the ${data.license} license.
 
-    ## Questions
-    Please reach out for additional questions!
-    GitHub Username: ${data.username}
-    GitHub Page: https://github.com/${data.username}
-    Email: ${data.email}
-    `;
+## Questions
+Please reach out for additional questions!
+GitHub Username: ${data.username}
+GitHub Page: https://github.com/${data.username}
+Email: ${data.email}
+`;
 };
 
 module.exports = generateReadme;
